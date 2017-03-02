665d4505381dc3429fe1584ea092fd83edaf9ef5
Freyskeyd <simon.paitrault@gmail.com>
2017-03-02 14:09:26 +0100

    :rocket: init
    Signed-off-by: Freyskeyd <simon.paitrault@gmail.com>

---
+++ examples/test.rs
@@ -0,0 +1,29 @@
+extern crate xmpp;
+extern crate futures;
+extern crate tokio_core;
+
+use xmpp::connect_client;
+use xmpp::ClientMessage;
+use futures::sync::mpsc;
+use futures::{Sink, Stream};
+use std::thread;
+use tokio_core::reactor::Core;
+
+fn main() {
+    let (tx, rx) = mpsc::channel(1000);
+    let mut core = Core::new().expect("Failed to create core");
+    thread::spawn(move || {
+        connect_client(tx, |_| {
+            None
+        });
+    });
+
+    let xx = rx.for_each(|(_, mut in_tx)| {
+        let _ = in_tx.start_send(ClientMessage("<iq from='admin@bot.simon.iadvize.com' to='bot.simon.iadvize.com' id='c2s1' type='get'><ping xmlns='urn:xmpp:ping'/></iq>".to_string()));
+
+        Ok(())
+    });
+
+    core.run(xx).expect("Core failed to run");
+}
+
