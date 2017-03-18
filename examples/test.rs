c822721b571037889d4be6df94fd41447e933d2c
Freyskeyd <simon.paitrault@gmail.com>
2017-03-18 02:07:03 +0100

    up
    Signed-off-by: Freyskeyd <simon.paitrault@gmail.com>

--- examples/test.rs
+++ examples/test.rs
@@ -1,18 +1,7 @@
 extern crate xmpp;
 
 use xmpp::connect_client;
+
 fn main() {
     connect_client()
-    // let (tx, rx) = mpsc::channel(1);
-
-    // run_client(rx, tx, |ServerMessage(msg)| -> Option<ClientMessage> {
-    //     if msg.len() > 1 {
-    //         if msg.starts_with("<proceed") {
-    //             return Some(ClientMessage("<auth".to_string()));
-    //         }
-    //     }
-    //     None
-    // });
 }
-
-
