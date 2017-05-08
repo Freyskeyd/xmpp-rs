f4692405e96ebec5c939549328fb441649e6c11c
Freyskeyd <simon.paitrault@gmail.com>
2017-05-08 23:21:47 +0200

    feat(test): credentials
    Signed-off-by: Freyskeyd <simon.paitrault@gmail.com>

--- proto/src/credentials.rs
+++ proto/src/credentials.rs
@@ -11,7 +11,40 @@ impl Default for Credentials {
     fn default() -> Credentials {
         Credentials {
             jid: Jid::from_str("guest").unwrap(),
-            password: "guest".to_string(),
+            password: "guest".to_string()
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_default_values() {
+        let c = Credentials { ..Credentials::default() };
+
+        assert_eq!(c.jid, Jid::from_str("guest").unwrap());
+        assert_eq!(c.password, "guest");
+    }
+
+    #[test]
+    fn creation() {
+        let c = Credentials { jid: Jid::from_str("guest").unwrap(), password: "guest".into() };
+        assert_eq!(c.jid, Jid::from_str("guest").unwrap());
+        assert_eq!(c.password, "guest");
+    }
+
+    #[test]
+    fn equality() {
+        let c = Credentials { jid: Jid::from_str("guest").unwrap(), password: "guest".into() };
+        let mut d = c.clone();
+
+        assert!(c == d);
+
+        d.jid = Jid::from_str("guest2").unwrap();
+        d.password = "guest".into();
+
+        assert!(c != d);
     }
 }
