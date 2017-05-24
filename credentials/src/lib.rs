ce3ce81f9ff7b938fe5099bdb6d2405501b72438
Freyskeyd <simon.paitrault@gmail.com>
2017-05-24 10:58:08 +0200

    add credentials module
    Signed-off-by: Freyskeyd <simon.paitrault@gmail.com>

---
+++ credentials/src/lib.rs
@@ -0,0 +1,84 @@
+extern crate xmpp_jid;
+extern crate sasl;
+
+use xmpp_jid::Jid;
+use std::str::FromStr;
+use sasl::common::Credentials as SaslCredentials;
+
+/// Define Credentials used to authenticate a user
+#[derive(Clone,Debug,PartialEq)]
+pub struct Credentials {
+    pub jid: Jid,
+    pub password: String,
+}
+
+impl Credentials {
+    pub fn format(&self) -> SaslCredentials {
+        SaslCredentials::default()
+            .with_username(self.jid.to_string())
+            .with_password(self.password.to_string())
+    }
+}
+
+impl Default for Credentials {
+    fn default() -> Credentials {
+        Credentials {
+            jid: Jid::from_str("guest").unwrap(),
+            password: "guest".to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_default_values() {
+        let c = Credentials { ..Credentials::default() };
+
+        assert_eq!(c.jid, Jid::from_str("guest").unwrap());
+        assert_eq!(c.password, "guest");
+    }
+
+    #[test]
+    fn creation() {
+        let c = Credentials {
+            jid: Jid::from_str("guest").unwrap(),
+            password: "guest".into(),
+        };
+        assert_eq!(c.jid, Jid::from_str("guest").unwrap());
+        assert_eq!(c.password, "guest");
+    }
+
+    #[test]
+    fn equality() {
+        let c = Credentials {
+            jid: Jid::from_str("guest").unwrap(),
+            password: "guest".into(),
+        };
+        let mut d = c.clone();
+
+        assert!(c == d);
+
+        d.jid = Jid::from_str("guest2").unwrap();
+        d.password = "guest".into();
+
+        assert!(c != d);
+    }
+
+    #[test]
+    fn format() {
+        let c = Credentials {
+            jid: Jid::from_str("guest").unwrap(),
+            password: "guest".into(),
+        };
+
+        let check = SaslCredentials::default()
+            .with_username("guest")
+            .with_password("guest");
+
+        assert!(c.format().identity == check.identity);
+        assert!(c.format().secret == check.secret);
+    }
+}
