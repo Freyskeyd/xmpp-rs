6ebc995709f7e16fbf23b96cba07991726150b3e
Freyskeyd <simon.paitrault@gmail.com>
2021-05-11 21:58:31 +0200

    Added tests to auth and starttls
    Signed-off-by: Freyskeyd <simon.paitrault@gmail.com>

--- proto/src/non_stanza/auth.rs
+++ proto/src/non_stanza/auth.rs
@@ -28,14 +28,43 @@ impl FromXmlElement for Auth {
 
 #[cfg(test)]
 mod tests {
+    use std::io::Write;
+
+    use circular::Buffer;
+    use xmpp_xml::xml::{reader::XmlEvent, ParserConfig};
+
     use super::*;
+
+    const EXPECTED_AUTH: &'static str = "<auth xmlns='urn:ietf:params:xml:ns:xmpp-sasl' mechanism='PLAIN'>AGp1bGlldAByMG0zMG15cjBtMzA=</auth>";
 
     #[test]
     fn parse_proceed_plain() {
-        let element = Element::from_reader("<auth xmlns='urn:ietf:params:xml:ns:xmpp-sasl' mechanism='PLAIN'>AGp1bGlldAByMG0zMG15cjBtMzA=</auth>".as_bytes()).unwrap();
+        let element = Element::from_reader(EXPECTED_AUTH.as_bytes()).unwrap();
         let proceed = Auth::from_element(element).unwrap();
 
         assert_eq!(proceed.mechanism.unwrap(), "PLAIN");
         assert_eq!(proceed.challenge.unwrap(), "AGp1bGlldAByMG0zMG15cjBtMzA=");
+    }
+
+    #[test]
+    fn from() {
+        let mut cfg = ParserConfig::new().whitespace_to_characters(true);
+        cfg.ignore_end_of_stream = true;
+        let mut reader = cfg.create_reader(Buffer::with_capacity(4096));
+
+        reader.source_mut().write(EXPECTED_AUTH.as_bytes()).unwrap();
+        let _ = reader.next().unwrap();
+        let x = reader.next().unwrap();
+
+        assert!(matches!(x, XmlEvent::StartElement { .. }));
+
+        if let XmlEvent::StartElement { name, attributes, namespace } = x {
+            let packet = Packet::parse(&mut reader, name, namespace, attributes);
+            assert!(
+                matches!(packet, Ok(Packet::NonStanza(ref stanza)) if matches!(**stanza, NonStanza::Auth(_))),
+                "Packet wasn't an Auth, it was: {:?}",
+                packet
+            );
+        }
     }
 }
--- proto/src/non_stanza/start_tls.rs
+++ proto/src/non_stanza/start_tls.rs
@@ -21,3 +21,37 @@ impl FromXmlElement for StartTls {
         Ok(Self {})
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::io::Write;
+
+    use circular::Buffer;
+    use xmpp_xml::xml::{reader::XmlEvent, ParserConfig};
+
+    use super::*;
+
+    const EXPECTED_STARTTLS: &'static str = "<starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>";
+
+    #[test]
+    fn from() {
+        let mut cfg = ParserConfig::new().whitespace_to_characters(true);
+        cfg.ignore_end_of_stream = true;
+        let mut reader = cfg.create_reader(Buffer::with_capacity(4096));
+
+        reader.source_mut().write(EXPECTED_STARTTLS.as_bytes()).unwrap();
+        let _ = reader.next().unwrap();
+        let x = reader.next().unwrap();
+
+        assert!(matches!(x, XmlEvent::StartElement { .. }));
+
+        if let XmlEvent::StartElement { name, attributes, namespace } = x {
+            let packet = Packet::parse(&mut reader, name, namespace, attributes);
+            assert!(
+                matches!(packet, Ok(Packet::NonStanza(ref stanza)) if matches!(**stanza, NonStanza::StartTls(_))),
+                "Packet wasn't an StartTls, it was: {:?}",
+                packet
+            );
+        }
+    }
+}
