39152492d74f0685fc4b2d420132e102d51f4a90
Freyskeyd <freyskeyd@gmail.com>
2021-05-11 07:22:22 +0000

    Deploying to gh-pages from  @ c07cfa91ff67f4a256d9610224a4163eb2014186 🚀

---
+++ implementors/core/convert/trait.TryFrom.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["xmpp_proto"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"struct\" href=\"xmpp_xml/element/struct.Element.html\" title=\"struct xmpp_xml::element::Element\">Element</a>&gt; for <a class=\"enum\" href=\"xmpp_proto/enum.Packet.html\" title=\"enum xmpp_proto::Packet\">Packet</a>","synthetic":false,"types":["xmpp_proto::packet::Packet"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
