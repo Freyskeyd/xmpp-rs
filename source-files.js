8c3e19aba838b302697f66cd59c1d953fcce0379
Freyskeyd <freyskeyd@gmail.com>
2021-05-11 20:25:11 +0000

    Deploying to gh-pages from  @ dbfce5c788c82f4311874859930bfb8ca33f0fae 🚀

--- source-files.js
+++ source-files.js
@@ -2,7 +2,7 @@ var N = null;var sourcesIndex = {};
 sourcesIndex["server"] = {"name":"","files":["main.rs"]};
 sourcesIndex["xmpp"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["xmpp_credentials"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["xmpp_proto"] = {"name":"","dirs":[{"name":"non_stanza","files":["auth.rs","open_stream.rs","proceed_tls.rs","start_tls.rs","stream_features.rs"]},{"name":"stanza","files":["generic_iq.rs"]}],"files":["lib.rs","non_stanza.rs","ns.rs","packet.rs","stanza.rs"]};
+sourcesIndex["xmpp_proto"] = {"name":"","dirs":[{"name":"non_stanza","files":["auth.rs","open_stream.rs","proceed_tls.rs","sasl_success.rs","start_tls.rs","stream_features.rs"]},{"name":"stanza","files":["generic_iq.rs"]}],"files":["lib.rs","non_stanza.rs","ns.rs","packet.rs","stanza.rs"]};
 sourcesIndex["xmpp_server"] = {"name":"","dirs":[{"name":"listeners","files":["tcp.rs","ws.rs"]}],"files":["lib.rs","listeners.rs","parser.rs","router.rs"]};
 sourcesIndex["xmpp_xml"] = {"name":"","files":["children.rs","element.rs","error.rs","lib.rs","namespace.rs","options.rs","position.rs","qname.rs","xml_atom.rs"]};
 createSourceSidebar();
